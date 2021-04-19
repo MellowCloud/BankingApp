43615670f366c1cd7e5d782e46042b8bdc8cfa69
MellowCloud <joeykitz25@yahoo.com>
2021-04-18 20:50:48 -0600

    update to methods an names

--- BankingApp.cr
+++ BankingApp.cr
@@ -6,19 +6,70 @@ class Bank
     #initializes the new Bank
     def initialize
         @name = "Bank of Doves"
-        @accounts = [] of Account
+        @customers = [] of Customers
+        @accrueRate = 0.02
     end
 
-    def setName(name : String)
+    #Changes the name of the bank from the default
+    def set_name(name : String)
         @name = name
     end
 
-    def getName
+    #Returns the name of the bank
+    def get_name
         @name
+    end
+
+    #Changes the Bank's accrue rate
+    def set_accrue_rate(rate : Float64)
+        @accrueRate = rate
+    end
+
+    def get_accrue_rate
+        @accrueRate
     end
 
 end
 
-class Account
-    
+module Account
+    def get_balance
+        @balance
+    end
+
+    def set_balance(bal : Float64)
+        @balance = bal
+    end
+
+    def deposit(dep : Float64)
+        @balance += dep
+    end
+
+    def withdraw(wit : Float64)
+        @balance -= wit
+    end
+end
+
+class SavingsAccount
+    include Account
+
+end
+
+class CheckingAccount
+    include Account
+
+    def initialize
+        @balance = 0
+    end
+end
+
+class Customers
+    def initialize
+        @name = "Nameless"
+        @CheckingAccount
+        @SavingsAccount
+    end
+
+    def set_name
+        
+    end
 end
